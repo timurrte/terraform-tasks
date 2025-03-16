c019172bab8672e18703a49b1590f1be0640a63d
Tymur Nikolaiev <tymurnikolaiev@ukr.net>
2025-03-16 22:53:19 +0200

    Added description to outputs

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,11 +1,14 @@
 output "rg_id" {
-  value = azurerm_resource_group.rg01.id
+  value       = azurerm_resource_group.rg01.id
+  description = "Azure Resource Group ID"
 }
 
 output "sa_blob_endpoint" {
-  value = azurerm_storage_account.st01.primary_blob_endpoint
+  value       = azurerm_storage_account.st01.primary_blob_endpoint
+  description = "Azure Storage Account blob service primary endpoint"
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet01.id
+  value       = azurerm_virtual_network.vnet01.id
+  description = "VNet ID"
 }
