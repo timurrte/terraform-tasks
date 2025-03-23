7e301a47f5889744edfed496bd9fdacbf76e6531
Tymur Nikolaiev <tymurnikolaiev@ukr.net>
2025-03-23 17:09:25 +0200

    Formatted properly SQL connection string

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -6,7 +6,7 @@ output "sql_connection_string" {
   description = "SQL connection string"
   sensitive   = true
   value = format(
-    "Server=tcp:%s.database.windows.net,1433;Initial Catalog=%s;Persist Security Info=False;User ID={sql_user_name};Password={sql_user_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+    "Server=tcp:%s.database.windows.net,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
     azurerm_mssql_server.server.name,
     azurerm_mssql_database.db.name,
     var.sql.admin_username,
