433196d8ff36d8fe11202f95b16f4d8bcc89bfaa
Tymur Nikolaiev <tymurnikolaiev@ukr.net>
2025-04-30 20:52:24 +0300

    Added outputs to Redis module

--- task08/main.tf
+++ task08/main.tf
@@ -107,14 +107,14 @@ data "azurerm_key_vault_secret" "redis_host" {
   name         = var.redis_host_secret_name
   key_vault_id = module.kv.id
 
-  depends_on = [module.redis]
+  depends_on = [module.redis, module.redis.pak, module.redis.host]
 }
 
 data "azurerm_key_vault_secret" "redis_pwd" {
   name         = var.redis_pak_secret_name
   key_vault_id = module.kv.id
 
-  depends_on = [module.redis]
+  depends_on = [module.redis, module.redis.pak, module.redis.host]
 }
 
 resource "kubectl_manifest" "secret_provider" {
---
+++ task08/modules/redis/outputs.tf
@@ -0,0 +1,9 @@
+output "pak" {
+  description = "Redis PAK name"
+  value       = azurerm_key_vault_secret.pak
+}
+
+output "host" {
+  description = "Redis host secret name"
+  value       = azurerm_key_vault_secret.host
+}
