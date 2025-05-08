3a24fa01a58988a8107f1f3ff4eb8a8afa1ab502
Tymur Nikolaiev <timurnikolaus@gmail.com>
2025-05-08 21:15:13 +0300

    Minor style changes

--- task09/modules/keyvault/main.tf
+++ task09/modules/keyvault/main.tf
@@ -17,7 +17,8 @@ data "azurerm_client_config" "current" {
 resource "azurerm_key_vault_access_policy" "example" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+
+  object_id = data.azurerm_client_config.current.object_id
 
   key_permissions = [
     "Get", "List"
